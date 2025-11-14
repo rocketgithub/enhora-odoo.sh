65a34062a4f2c4b2fbb2d36aa93e90c755cf73b4
Rodrigo Fernandez <rodrigo@aquih.com>
2025-11-14 10:41:36 -0600

    [ADD] Agregar modulos

---
+++ sh_export_product_image/__manifest__.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+# Part of Softhealer Technologies.
+{
+    "name": "Export Images in zip",
+    "author": "Softhealer Technologies",
+    "website": "https://www.softhealer.com",
+    "support": "support@softhealer.com",
+    "category": "Extra Tools",
+    "license": "OPL-1",
+    "summary": """
+Export Bulk of Images In zip,
+Export Mass Images Zip Module,
+Product Icons In Zip App,
+Export Several Pic In Zip,
+Export Bulk Photograph In Zip,
+Export Picture In Zip,
+Mass Image Export,
+Bulk Photos Export Odoo
+""",
+    "description": """
+This module will provide a feature to export
+product images in zip files on a single click.
+Export Bulk of Images In zip Odoo
+Export Mass Images In Zip Module,
+Put Product Icons In Zip,
+Export Several Pic In Zip,
+Export Bulk Photograph In Zip,
+Export Picture In Zip Odoo.
+Export Mass Images Zip Module,
+Product Icons In Zip App,
+Export Several Pic In Zip,
+Export Bulk Photograph In Zip,
+Export Picture In Zip Odoo.
+""",
+    "version": "16.0.1",
+    "depends": [
+        "sale_management",
+    ],
+    "application": True,
+    "data": [
+        "security/sh_export_product_image_groups.xml",
+        "security/ir.model.access.csv",
+        "wizard/sh_export_product_image_product_views.xml",
+        "wizard/sh_export_product_image_template_views.xml",
+    ],
+    "images": ["static/description/background.png", ],
+    "live_test_url": "https://youtu.be/cTlUCUb_Tx4",
+    "auto_install": False,
+    "installable": True,
+    "price": 25,
+    "currency": "EUR"
+}
